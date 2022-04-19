e0b53ac12a81e6aaf5fa7b18bbca78f364beca01
Sergey Balandin <sergbalinf@yandex.ru>
2022-04-19 22:41:59 +0300

    fix замечания

--- scripts/index.js
+++ scripts/index.js
@@ -46,8 +46,8 @@ let elementCard = templateCards.querySelector('.element');
 let cardAddButton = document.querySelector('.profile__add');
 let popupCardWindow = document.querySelector('.popup_value_add-place');
 let popupCloseCard = document.querySelector('.popup__close_card-window');
-let addCardName = popupCardWindow.querySelector('.popup__add-form_name');
-let addCardLink = popupCardWindow.querySelector('.popup__add-form_link');
+let addCardName = popupCardWindow.querySelector('.popup__addform_name');
+let addCardLink = popupCardWindow.querySelector('.popup__addform_link');
 let newCardForm = popupCardWindow.querySelector('.popup__add-form');
 
 // Открытие изображения
